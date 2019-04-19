0e6a9942a092a8f42d8ce60a8f2cedf7579309c4
lu wei <wlu27@best-inc.com>
2019-04-19 15:19:26 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2017-present, AloesLu. All rights reserved.
+ *
+ * Best Shop
+ * app.js
+ */
+
+// ===== MODULES ===============================================================
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import {Server} from 'http';
+
+// ===== ROUTES ================================================================
+// import index from './routes/index';
+// import lists from './routes/lists';
+import webhooks from './routes/webhooks';
+
+/* =============================================
+   =                Initialize                 =
+   ============================================= */
+
+export const app = express()
+export const server = Server(app);
+
+/* =============================================
+   =           Basic Configuration             =
+   ============================================= */
+
+/* ----------  Views  ---------- */
+
+/* ----------  Static Assets  ---------- */
+
+/* ----------  Parsers  ---------- */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+/* ----------  Loggers &c  ---------- */
+
+
+/* =============================================
+   =                   Sockets                 =
+   ============================================= */
+
+/* ----------  Sockets  ---------- */
+
+/* ----------  Sockets Hooks  ---------- */
+
+
+/* =============================================
+   =                   Routes                  =
+   ============================================= */
+
+/* ----------  Primary / Happy Path  ---------- */
+app.use('/webhook', webhooks);
+
+/* ----------  Errors  ---------- */
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+   
+// development error handler, will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+      new Error(err); // eslint-disable-line no-new
+    });
+}
+
+// production error handler, no stacktraces leaked to user
+app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+    message: err.message,
+    error: {},
+    });
+});   
+
+/* =============================================
+   =              Messenger Setup              =
+   ============================================= */
+
+
+   
+/* =============================================
+      =            Complete Configuration         =
+      ============================================= */
+
+const appPort = process.env.PORT;
+server.listen(appPort);
+
+export default app;
